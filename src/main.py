536233d1e7c65ed31222d4c35c60ddc15b485806
Alexey-Nikolaevic-B <alexey.bardushko@gmail.com>
2024-08-23 19:04:01 +0300

    basic testing

--- src/main.py
+++ src/main.py
@@ -1,48 +1,160 @@
-import sys
-from PyQt5 import QtWidgets, QtGui
+import sys, os
+import re
+from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtWidgets import QDialog, QApplication
 from PyQt5.uic import loadUi
 
+import cmd_interface
+
+
+class AppData():
+    vzvod = 0
+    name = ""
+    mark = 0
+    task = []
+    check = []
+    cur_task = 0
+
 
 class CMDScreen(QDialog):
-    def __init__(self):
+    appData = AppData()
+    input_cmd = ''
+    def __init__(self, input: AppData):
+        self.appData = input
         super().__init__()
+        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
         
         loadUi('qt\CMD.ui', self)
         self.setWindowIcon(QtGui.QIcon('img/icon_cmd.jpg'))
         self.setWindowTitle('Администратор: Командная строка')
 
+        self.label.setText(os.getcwd() + ">")
+        self.lineEdit.returnPressed.connect(self.enter)
+
+    def enter(self):
+        self.input_cmd = self.lineEdit.text()
+        # result = cmd_interface.exec_command(self.input_cmd)
+        # self.label_2.setText()
+
+        check_pattern = re.compile(self.appData.check[self.appData.cur_task])
+
+        if check_pattern.match(self.input_cmd):
+            self.appData.mark = self.appData.mark + 1
+            self.label_2.setText("МОЛОДЕЦ")
+
 
 class TestScreen(QDialog):
-    def __init__(self):
+    appData = AppData()
+
+    def __init__(self, input: AppData):
+        self.appData = input
         super(TestScreen, self).__init__()
         loadUi('qt\Test.ui', self)
 
-        self.cmd = CMDScreen()
+        self.btn_backward.setIcon(QtGui.QIcon('img/btn_backward.png'))
+        self.btn_forward.setIcon(QtGui.QIcon('img/btn_forward.png'))
+        self.btn_backward.setIconSize(QtCore.QSize(100,40))
+        self.btn_forward.setIconSize(QtCore.QSize(100,40))
+        style = "QPushButton { font: 75 14pt \"MS Shell Dlg 2\"; } QPushButton::hover { background-color : rgb(197, 255, 149)}"
+        self.btn_backward.setStyleSheet(style) 
+        self.btn_forward.setStyleSheet(style) 
+        self.btn_end_test.setStyleSheet(style) 
+                             
+
+        self.btn_backward.setEnabled(False)
+        self.btn_end_test.hide()
+
+        self.cmd = CMDScreen(self.appData)
         self.cmd.show()
+
+        self.lbl_task_num.setText("Задание " + str(self.appData.cur_task + 1))
+        self.lbl_task.setText(str(self.appData.task[self.appData.cur_task]))
+
+        self.btn_end_test.clicked.connect(self.gotoReslutScreen)
+
+        self.btn_backward.clicked.connect(lambda: self.update(0))
+        self.btn_forward.clicked.connect(lambda: self.update(1))
+
+    def update(self, option):
+        if (option == 0) and (self.appData.cur_task > 0): 
+            self.appData.cur_task = self.appData.cur_task - 1
+        if (option == 1) and (self.appData.cur_task < len(self.appData.task) - 1):
+            self.appData.cur_task = self.appData.cur_task + 1
+
+        self.btn_backward.setEnabled(True)
+        self.btn_forward.setEnabled(True)
+        self.btn_end_test.hide()
+        if (self.appData.cur_task < 1):
+            self.btn_backward.setEnabled(False)
+        if (self.appData.cur_task == len(self.appData.task) - 1):
+            self.btn_forward.setEnabled(False)
+            self.btn_end_test.show()
+
+        self.lbl_task_num.setText("Задание " + str(self.appData.cur_task + 1))
+        self.lbl_task.setText("\t" + str(self.appData.task[self.appData.cur_task]))
+
+    def gotoReslutScreen(self):
+        self.cmd.close()
+        resultScreen = ResultScreen(self.appData)
+        widget.addWidget(resultScreen)
+        widget.setCurrentIndex(widget.currentIndex()+1)
 
 
 class LoginScreen(QDialog):
-    def __init__(self):
+    appData = AppData()
+
+    def __init__(self, input: AppData):
+        self.appData = input
         super(LoginScreen, self).__init__()
         loadUi('qt\Login.ui', self)
+
+        style = "QPushButton { font: 75 14pt \"MS Shell Dlg 2\"; } QPushButton::hover { background-color : rgb(197, 255, 149)}"
+        self.btn_start_test.setStyleSheet(style) 
 
         self.btn_start_test.clicked.connect(self.gotoTestScreen)
 
     def gotoTestScreen(self):
-        testScreen = TestScreen()
-        widget.addWidget(testScreen)
-        widget.setCurrentIndex(widget.currentIndex()+1)
+        data.vzvod_num = self.lbl_vzvod_num.text()
+        data.name = self.lbl_student_name.text()
+        if (len(data.vzvod_num) == 0 or len(data.name) == 0):
+            self.lbl_error.setText("Введите данные")
+        else:
+            testScreen = TestScreen(self.appData)
+            widget.addWidget(testScreen)
+            widget.setCurrentIndex(widget.currentIndex()+1)
+
+
+class ResultScreen(QDialog):
+    appData = AppData()
+    def __init__(self, input: AppData):
+        self.appData = input
+        super(ResultScreen, self).__init__()
+        loadUi('qt\Result.ui', self)
+
+        self.lbl_result.setText(str(self.appData.mark))
+        self.lbl_result1.setText(str(self.appData.mark) + " / " + str(len(self.appData.task)))
+
+        if (self.appData.mark == 2):
+            self.setStyleSheet("background-color: rgb(255, 117, 117);")
+        elif (self.appData.mark == 3):
+            self.setStyleSheet("background-color: rgb(255, 184, 96);")
+        elif (self.appData.mark == 4):
+            self.setStyleSheet("background-color: rgb(243, 255, 135);")
+        elif (self.appData.mark == 5):
+            self.setStyleSheet("background-color: rgb(85, 255, 127);")
 
 
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
+    data = AppData()
+    data.task = ["Узнайте имя текущего пользователя", "Узнайте содержание текщей папки", "Узнайте содержание папки"]
+    data.check = ["[ ]*whoami[ ]*", "[ ]*cd[ ]*", "[ ]*dir[ ]*"]
 
-    loginScreen = LoginScreen()
+    loginScreen = LoginScreen(data)
     widget = QtWidgets.QStackedWidget()
     widget.addWidget(loginScreen)
-    widget.show()
+    widget.showFullScreen()
 
     try:
         sys.exit(app.exec_())
